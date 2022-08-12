3bbcfa6ca2d968e508eb1cc2292cc5df68f9d19f
Vijitha Epa <vijithaepa@gmail.com>
2022-08-12 22:41:46 +1000

    member list

--- src/components/members/MemberList.js
+++ src/components/members/MemberList.js
@@ -2,7 +2,19 @@ import classNames from 'classnames';
 import Avatar from 'components/common/Avatar';
 import Flex from 'components/common/Flex';
 import React, { useState } from 'react';
-import { Badge, Card, Col, Form, ProgressBar, Row, Table } from 'react-bootstrap';
+import {
+  Badge,
+  Button,
+  Card,
+  Col,
+  Form,
+  ListGroup,
+  ListGroupItem,
+  Modal,
+  ProgressBar,
+  Row,
+  Table
+} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import SoftBadge from 'components/common/SoftBadge';
 import PropTypes from 'prop-types';
@@ -19,6 +31,7 @@ import product10 from "../../assets/img/products/10.png";
 import team1 from "../../assets/img/team/1.jpg";
 import './Members.css'
 import Rating from "react-rating";
+import { CloseButton } from "../common/Toast";
 
 const getColorByRating = (number) => {
   if (number > 80)
@@ -52,147 +65,214 @@ const MembersRow = ({
                       isLast
                     }) => {
   const bgColor = getColorByRating(rating * 10)
+  // const [memberId, setMemberId] = useState(id)
+  const [show, setShow] = useState(false)
+
   console.log("BG Color ", id, gender, gender === 'm')
   return (
-    <tr className={classNames({'border-bottom border-200': !isLast})}>
-      <td>
-        <Flex alignItems="center" className="position-relative">
-          <Avatar
-            className={`status-online`} // status-offline, status-away
-            size="4xl"
-            src={img}
-            width="60"
-            alt={name}
-          />
-          <div className="flex-1 ms-3">
-            <h6 className="mb-0 fw-semi-bold">
-              <Link className="text-dark stretched-link" to="#!" key={id}>
-                {name}
-              </Link>
-            </h6>
-            {/*<p className = "fw-semi-bold mb-0 text-500">*/}
-            <Badge bg={gender === 'm' ? "primary": 'warning'}>
-              {gender}
-            </Badge>
-            <SoftBadge bg={"secondary"}>
-              {age}
-            </SoftBadge>
-            {/*</p>*/}
+    <>
 
-            {/*<p className="fs--2 mb-0 text-500">{gender}/{age}</p>*/}
-          </div>
-        </Flex>
-      </td>
-      <td className="align-middle text-center fw-semi-bold">
-        {edu_qualifications.map((ed, index) =>
-          <SoftBadge pill className={"me-2"} bg={"primary"} key={index}>
-            {ed}
-          </SoftBadge>)}
 
-      </td>
-      <td className="align-middle text-center fw-semi-bold">
-        {prof_qualifications.map((pro, index) =>
-          <SoftBadge pill className={"me-2"} bg={"primary"} key={index}>
-            {pro}
-          </SoftBadge>
-        )}
-
-        {/*<p className="fs--2 mb-0">{10}</p>*/}
-      </td>
-      <td className="align-middle">
-        <Row className="g-2 justify-content-center">
-          <Col xs="auto">
-            <Badge className={"me-2 " + bgColor}>
-              {rating * 10}%
-            </Badge>
-            <SoftBadge pill bg={"primary"}>
-              by {total_votes} votes
-            </SoftBadge>
-            <Flex alignItems="center">
-              <Rating initialRating={Math.abs(rating)} fractions={2} readonly stop={10}
-                      className="hint--bottom hint--bounce" aria-label="Current rating"
-                      emptySymbol={<FontAwesomeIcon icon={['far', 'star']} className="text-warning" />}
-                      placeholderSymbol={<FontAwesomeIcon icon="star" className="text-danger" />}
-                      fullSymbol={<FontAwesomeIcon icon="star" className="text-warning" />}
-              />
-              {/*<ProgressBar now={Math.abs(rating) * 10} style={{width: '80px', height: 5}} variant={rating > 0? 'info': 'warning'} />*/}
-            </Flex>
-            {/*<p className="fs--2 mb-0">{'1h:52m'}</p>*/}
-          </Col>
-        </Row>
-      </td>
-      <td className="align-middle text-center fw-semi-bold">
-        <Row className="g-2 justify-content-center">
-          <Col xs="auto" className="mt-auto">
-            <BasicECharts
-              echarts={echarts}
-              options={{
-                color: getColor('primary'),
-                tooltip: {show: false},
-                series: [{
-                  data: rating_history
-                }]
-              }}
-              className="mb-1"
-              style={{width: '10.625rem', height: '1rem'}}
+      <tr className={classNames({'border-bottom border-200': !isLast})}>
+        <td>
+          <Flex alignItems="center" className="position-relative">
+            <Avatar
+              className={`status-online`} // status-offline, status-away
+              size="4xl"
+              src={img}
+              width="60"
+              alt={name}
             />
-          </Col>
-        </Row>
-      </td>
-    </tr>
+            <div className="flex-1 ms-3">
+              <h6 className="mb-0 fw-semi-bold">
+                {/*<Link className="text-dark stretched-link" to={"member/memberDetails/" + id} key={id}>*/}
+                <Badge bg={"secondary"} onClick={() => setShow(true)}>  {name} </Badge>
+                {/*</Link>*/}
+              </h6>
+              {/*<p className = "fw-semi-bold mb-0 text-500">*/}
+              <Badge bg={gender === 'm' ? "primary" : 'warning'}>
+                {gender}
+              </Badge>
+              <SoftBadge bg={"secondary"}>
+                {age}
+              </SoftBadge>
+              {/*</p>*/}
+
+              {/*<p className="fs--2 mb-0 text-500">{gender}/{age}</p>*/}
+            </div>
+          </Flex>
+        </td>
+        <td className="align-middle text-center fw-semi-bold">
+          {edu_qualifications.map((ed, index) =>
+            <SoftBadge pill className={"me-2"} bg={"primary"} key={index}>
+              {ed}
+            </SoftBadge>)}
+
+        </td>
+        <td className="align-middle text-center fw-semi-bold">
+          {prof_qualifications.map((pro, index) =>
+            <SoftBadge pill className={"me-2"} bg={"primary"} key={index}>
+              {pro}
+            </SoftBadge>
+          )}
+
+          {/*<p className="fs--2 mb-0">{10}</p>*/}
+        </td>
+        <td className="align-middle">
+          <Row className="g-2 justify-content-center">
+            <Col xs="auto">
+              <Badge className={"me-2 " + bgColor}>
+                {rating * 10}%
+              </Badge>
+              <SoftBadge pill bg={"primary"}>
+                by {total_votes} votes
+              </SoftBadge>
+              <Flex alignItems="center">
+                <Rating initialRating={Math.abs(rating)} fractions={2} readonly stop={10}
+                        className="hint--bottom hint--bounce" aria-label="Current rating"
+                        emptySymbol={<FontAwesomeIcon icon={['far', 'star']} className="text-warning" />}
+                        placeholderSymbol={<FontAwesomeIcon icon="star" className="text-danger" />}
+                        fullSymbol={<FontAwesomeIcon icon="star" className="text-warning" />}
+                />
+                {/*<ProgressBar now={Math.abs(rating) * 10} style={{width: '80px', height: 5}} variant={rating > 0? 'info': 'warning'} />*/}
+              </Flex>
+              {/*<p className="fs--2 mb-0">{'1h:52m'}</p>*/}
+            </Col>
+          </Row>
+        </td>
+        <td className="align-middle text-center fw-semi-bold">
+          <Row className="g-2 justify-content-center">
+            <Col xs="auto" className="mt-auto">
+              <BasicECharts
+                echarts={echarts}
+                options={{
+                  color: getColor('primary'),
+                  tooltip: {show: false},
+                  series: [{
+                    data: rating_history
+                  }]
+                }}
+                className="mb-1"
+                style={{width: '10.625rem', height: '1rem'}}
+              />
+            </Col>
+          </Row>
+        </td>
+      </tr>
+
+      <Modal show={show} size="lg" fullscreen={"lg-down"} onHide={() => setShow(false)}>
+        <Modal.Header>
+          <Modal.Title>Member Details</Modal.Title>
+          <CloseButton
+            className="btn btn-circle btn-sm transition-base p-0"
+            onClick={() => setShow(false)}
+          />
+        </Modal.Header>
+        <Modal.Body>
+          <Row className="g-0 flex-between-center">
+            <Col md="auto">
+              <Card style={{width: '23.5rem'}}>
+                <Card.Img src={img} variant='top' />
+                <Card.Body>
+                  <Card.Title>{name}</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem onClick={() => console.log("Clicked ss")}><a href="#">Cras justo
+                    odio</a></ListGroupItem>
+                  <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                  <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                  <Card.Body>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                  </Card.Body>
+                </ListGroup>
+              </Card></Col>
+            <Col md="auto">
+              <Card style={{width: '23.5rem'}}>
+                <Card.Body>
+                  <Card.Title>{name}</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem>Cras justo odio</ListGroupItem>
+                  <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                  <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                  <Card.Body>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                  </Card.Body>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShow(false)}>
+            Close
+          </Button>
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
 const MemberList = () => {
 
-    const [memberList] = useState(members)
-    // console.log("Members ", memberList)
-    return (
-      <Card className="h-100">
-        <Card.Body className="p-0">
-          <SimpleBarReact>
-            <Table
-              borderless
-              className="mb-0 fs--1 border-200 rounded-3 table-dashboard table-member-info"
-            >
-              <thead className="bg-light">
-              <tr className="text-900">
-                <th>Member info</th>
-                <th className="text-center">Educational Qualifications</th>
-                <th className="text-center">Professional Qualifications</th>
-                <th className="text-center">Rating</th>
-                <th className="text-center">History</th>
-              </tr>
-              </thead>
-              <tbody>
-              {memberList.map((member, index) => (
-                <MembersRow
-                  {...member}
-                  isLast={index === memberList.length - 1}
-                  key={member.id}
-                />
-              ))}
-              </tbody>
-            </Table>
-          </SimpleBarReact>
-        </Card.Body>
+  const [memberList] = useState(members)
+  // console.log("Members ", memberList)
+  return (
+    <Card className="h-100">
+      <Card.Body className="p-0">
+        <SimpleBarReact>
+          <Table
+            borderless
+            className="mb-0 fs--1 border-200 rounded-3 table-dashboard table-member-info"
+          >
+            <thead className="bg-light">
+            <tr className="text-900">
+              <th>Member info</th>
+              <th className="text-center">Educational Qualifications</th>
+              <th className="text-center">Professional Qualifications</th>
+              <th className="text-center">Rating</th>
+              <th className="text-center">History</th>
+            </tr>
+            </thead>
+            <tbody>
+            {memberList.map((member, index) => (
+              <MembersRow
+                {...member}
+                isLast={index === memberList.length - 1}
+                key={member.id}
+              />
+            ))}
+            </tbody>
+          </Table>
+        </SimpleBarReact>
+      </Card.Body>
 
-        <Card.Footer className="bg-light py-2">
-          <Row className="g-0 flex-between-center">
-            <Col xs="auto">
-              <Form.Select size="sm" className="me-2">
-                <option>Last 7 days</option>
-                <option>Last Month</option>
-                <option>Last Year</option>
-              </Form.Select>
-            </Col>
-            <Col xs="auto">
-              <FalconLink title="View All" className="px-0" />
-            </Col>
-          </Row>
-        </Card.Footer>
-      </Card>
-    )
+      <Card.Footer className="bg-light py-2">
+        <Row className="g-0 flex-between-center">
+          <Col xs="auto">
+            <Form.Select size="sm" className="me-2">
+              <option>Last 7 days</option>
+              <option>Last Month</option>
+              <option>Last Year</option>
+            </Form.Select>
+          </Col>
+          <Col xs="auto">
+            <FalconLink title="View All" className="px-0" />
+          </Col>
+        </Row>
+      </Card.Footer>
+    </Card>
+  )
   // };
 };
 
